b4881fd07faa968ece6ae131f8409b0716cbd403
bass1121 <seanbass99@gmail.com>
2024-10-17 06:21:19 -0500

    Finished homepage, added images, linked stylesheet

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,52 @@
-import Metadata from "./layout";
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillGithub, AiFillLinkedin, AiFillFacebook} from 'react-icons/ai';
+import Image from 'next/image';
+import myEmoji from "../../public/images/myEmoji.png";
 
 export default function Home() {
   return (
     <div>
-      <main>
-       
+      <main className="bg-white px-10">
+        <section className="min-h-screen" >
+         
+          <nav className="py-10 mb-12 flex justify-between font-mono">
+            <h1 className="text-xl">Sean Bass</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+              </li>
+              <li>
+                <a href="#" className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 rounded-md ml-8">Resume</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-500 font-medium'>Sean Bass</h2>
+            <h3 className='text-2xl py-2'>Developer and Designer</h3>
+            <p className='text-md py-5 leading-8 text-gray-500'>
+              A Junior Developer with Full Stack Certification from Bottega University along with CS-50 certifications. I am self motivated, 
+              I have a growth oriented mindset and I value a postitive mindset in the worst situations. I enjoy designing websites, learning new things, working on a team, 
+              and finding solutions to problems.
+            </p>
+            <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+              <a><AiFillFacebook/></a>
+              <a><AiFillLinkedin/></a>
+              <a><AiFillGithub/></a>
+            </div>
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <Image className="homepage-img" src={myEmoji} alt="myEmoji" priority/>
+          </div>
+        </section>
+        {/* Second Page */}
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Services I offer</h3>
+          </div>
+        </section>
       </main>
     </div>
   );
 }
+
